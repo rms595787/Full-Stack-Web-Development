fb8c122f5c3e1d2a5e50e2756ea8e078eb24f796
Rahul Sharma <rahulsh3851@gmail.com>
2024-07-02 15:14:10 +0530

    new control flow learnings

---
+++ Full Stack JavaScript/Java Script by Huxn/My Notes/1. Beginners/3. Control Flow/2. Switch Statements/exercise/app.js
@@ -0,0 +1,21 @@
+// 1. Create variable name (fruit) & store "banana".
+// 2. If case is "Banana" print "Banana is good!"
+// 3. If case is "Orange" print "I am not a fan of orange."
+// 4. If case is "Apple" print "How you like them apples?"
+// 5. default "I  have never heard of that fruit."
+
+let fruit="orange";
+switch(fruit){
+    case "banana":
+        console.log("Banana is good!");
+        break;
+    case "orange":
+        console.log("I am not a fan of orange");
+        break;
+    case "apple":
+        console.log("How you like them apples?");
+        break;
+    default:
+        console.log("I have never heard of that fruit");
+        break;
+}
